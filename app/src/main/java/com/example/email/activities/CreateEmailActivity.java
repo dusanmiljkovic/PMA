018c70edd3f1a9f46d0491537b1bfbaba41acbdc
Dusan Miljkovic <dusanmiljkovic_95@hotmail.com>
2022-02-25 00:12:31 +0100

    Added create email and email activity

--- app/src/main/java/com/example/email/activities/CreateEmailActivity.java
+++ app/src/main/java/com/example/email/activities/CreateEmailActivity.java
@@ -1,16 +1,53 @@
 package com.example.email.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.email.R;
 
+import java.util.Objects;
+
 public class CreateEmailActivity extends BaseActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_email);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        initToolbar();
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Compose");
+    }
+
+    private void initToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(view -> super.onBackPressed());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.new_email_options_menu, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_send) {
+            Toast.makeText(getApplicationContext(), "Send clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/email/activities/EmailActivity.java
+++ app/src/main/java/com/example/email/activities/EmailActivity.java
@@ -1,16 +1,69 @@
 package com.example.email.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.email.R;
 
+import java.util.Objects;
+
 public class EmailActivity extends BaseActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
+
+        getSupportActionBar().hide();
+        initToolbar();
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Email");
     }
+
+    private void initToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(view -> super.onBackPressed());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.email_options_menu, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_save:
+                Toast.makeText(getApplicationContext(), "Save clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_delete:
+                Toast.makeText(getApplicationContext(), "Delete clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_reply:
+                Toast.makeText(getApplicationContext(), "Reply clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_reply_all:
+                Toast.makeText(getApplicationContext(), "Reply all clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_forward:
+                Toast.makeText(getApplicationContext(), "Forward clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
